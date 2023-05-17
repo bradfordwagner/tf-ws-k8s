228f2d53c8fbd1b4d06d73c2e40eeef29ad9bb4c
bradfordwagner <wagner.bradford@gmail.com>
2023-05-16 22:30:49 -0400

    update

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,10 +1,1 @@
-provider "kind" {
-  kubeconfig = pathexpand("~/.kube/${var.input}")
-}
-
-module "hello_world" {
-  source = "/Users/bwagner/workspace/github/bradfordwagner/terraform/bradfordwagner.tf.m.k8s.kind"
-  # source = "git::https://github.com/bradfordwagner/tf-m-k8s-kind.git?ref=1.0.0"
-  name = var.input
-}
-
+../../kind_main.tf
