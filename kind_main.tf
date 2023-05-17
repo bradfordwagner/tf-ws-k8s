decbeda98b025c99e4ab1db59cb4a137d0e18745
bradfordwagner <wagner.bradford@gmail.com>
2023-05-16 22:46:12 -0400

    add all potential fields

--- kind_main.tf
+++ kind_main.tf
@@ -3,7 +3,9 @@ provider "kind" {
 }
 
 module "kind_cluster" {
-  source = "git::https://github.com/bradfordwagner/tf-m-k8s-kind.git?ref=0.1.0"
-  name = var.cluster_name
+  source              = "git::https://github.com/bradfordwagner/tf-m-k8s-kind.git?ref=0.1.0"
+  extra_port_mappings = var.extra_port_mappings
+  k8s_image           = var.k8s_image
+  name                = var.cluster_name
 }
 
--- kind_variables.tf
+++ kind_variables.tf
@@ -1,3 +1,17 @@
 variable "cluster_name" {
   type = string
 }
+
+variable "extra_port_mappings" {
+  default = []
+  type = list(object({
+    host_port      = number
+    container_port = number
+    protocol       = string # TCP
+  }))
+}
+
+variable "k8s_image" {
+  default = "kindest/node:v1.25.8"
+}
+
